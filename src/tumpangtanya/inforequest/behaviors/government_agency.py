532f5d0fbcbcee52a509b165d4ef820f980520d1
Khairil Yusof <khairil.yusof@gmail.com>
2022-02-24 14:26:40 +0800

    Better widget

--- src/tumpangtanya/inforequest/behaviors/government_agency.py
+++ src/tumpangtanya/inforequest/behaviors/government_agency.py
@@ -2,10 +2,12 @@
 
 from plone import schema
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.autoform import directives
 from plone.supermodel import model
 from Products.CMFPlone.utils import safe_hasattr
 from zope.component import adapter
 from zope.interface import Interface, implementer, provider
+from plone.app.z3cform.widget import RelatedItemsFieldWidget, SelectFieldWidget
 
 from tumpangtanya.inforequest import _
 
@@ -18,10 +20,16 @@ class IGovernmentAgencyMarker(Interface):
 class IGovernmentAgency(model.Schema):
     """ """
 
-    project = schema.TextLine(
-        title=_(u"Project"),
-        description=_(u"Give in a project name"),
+    directives.widget(government_agency=SelectFieldWidget)
+    government_agency = schema.Choice(
+        title=_(u"Government Agency"),
+        description=_(
+            u"""
+        Government Agency this request was made to
+        """
+        ),
         required=False,
+        vocabulary="tumpangtanya.inforequest.GovernmentAgency",
     )
 
 
@@ -32,11 +40,11 @@ class GovernmentAgency(object):
         self.context = context
 
     @property
-    def project(self):
-        if safe_hasattr(self.context, "project"):
-            return self.context.project
+    def government_agency(self):
+        if safe_hasattr(self.context, "government_agency"):
+            return self.context.government_agency
         return None
 
-    @project.setter
-    def project(self, value):
-        self.context.project = value
+    @government_agency.setter
+    def government_agency(self, value):
+        self.context.government_agency = value
